7ca7b06ac9ab469c46d0262055d298d6aef5ecea
JerryPeter <jerry.peter@gmail.com>
2021-03-12 23:23:29 +0700

    Jumat Malam

---
+++ app.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+const models = require('./models');
+
+
+const user = {
+  firstName: "John",
+  lastName: "due",
+  email: "john@due.com",
+}
+
+models.User.create(user).then(result => {
+  console.log("Success");  
+}).catch(error => {
+  console.log("Failed");
+});
