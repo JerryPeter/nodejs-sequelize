68bafc149a1befe169c219efb9b353febaf16751
JerryPeter <jerry.peter@gmail.com>
2021-03-21 22:37:59 +0700

    Update Minggu

--- users.js
+++ users.js
@@ -12,27 +12,39 @@ const password = 'qwerty';
 
 const otherPassword = 'not_bacon';
 
-bcrypt.hash(password, saltRounds, function(err, hash) {
-    console.log(hash);
-    bcrypt.compare(password, hash).then(function(result) {
-        console.log(result);
-    }).catch(function(error) {
-        console.log('error');
-    });
-});
 
+// function generatePassword(pass){
+//   const saltRounds = 10;
+//   bcrypt.hash(pass, saltRounds, function(err, hash) {
+//     console.log(hash);
+//     return hash;    
+//   });
+// }
 
+// console.log(generatePassword("helloworld"));
 
+// bcrypt.hash(password, saltRounds, function(err, hash) {
+//   console.log(hash);
+//   bcrypt.compare(password, hash).then(function(result) {
+//       console.log(result);
+//   }).catch(function(error) {
+//       console.log('error');
+//   });
+// });
 
 var i;
-for (i = 0; i < 1000; i++) {
+for (i = 0; i < 2; i++) {
 
-    
-    const user = {
+    email = faker.internet.email();
+    bcrypt.hash(email, saltRounds, function(err, hash) {
+      console.log(hash);
+
+      var _email = mail;
+      const user = {
         firstName: faker.name.firstName(),
         lastName: faker.name.lastName(),
-        email: faker.internet.email(),
-        password: 'password'
+        email: _email,
+        password: hash
       }
       
       models.User.create(user).then(result => {
@@ -40,4 +52,7 @@ for (i = 0; i < 1000; i++) {
       }).catch(error => {
         console.log("Failed");
       });      
+
+    });
+      
 }
